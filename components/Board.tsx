51d0dbdeb9f6a54e0923cb6cbe95baa9dede197d
rhuanbello <rhuambello@gmail.com>
2023-05-30 21:01:15 -0300

    [feature] Drag and Drop Cards & Columns & Update DB

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,14 +6,16 @@ import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
 import { Column } from './Column';
 
 export const Board = () => {
-  const [board, getBoard, setBoardState] = useBoardStore(({
+  const [board, getBoard, setBoardState, updateBoard] = useBoardStore(({
     board,
     getBoard,
-    setBoardState
+    setBoardState,
+    updateBoard
   }) => [
       board,
       getBoard,
-      setBoardState
+      setBoardState,
+      updateBoard
     ])
 
   useEffect(() => {
@@ -30,12 +32,77 @@ export const Board = () => {
       const [removed] = entries.splice(source.index, 1);
       entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
-      setBoardState({
+
+      return setBoardState({
         ...board,
         columns: rearrangedColumns
       })
     }
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)]
+    const finishColIndex = columns[Number(destination.droppableId)]
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos
+    }
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos
+    }
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol.id === finishCol.id) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos
+      }
+
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol)
+
+      setBoardState({
+        ...board,
+        columns: newColumns
+      })
+
+    } else {
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos
+      }
+
+      newColumns.set(startCol.id, newCol)
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos
+      })
+
+
+      updateBoard(todoMoved, finishCol.id)
+      setBoardState({
+        ...board,
+        columns: newColumns
+      })
+    }
   }
+
+  useEffect(() => {
+    console.log(board)
+  }, [board]);
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,20 +1,35 @@
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
+import { database } from "@/appwrite";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void;
+  updateBoard: (todo: Todo, columnId: Status) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<Status, Column>(),
   },
+
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
     set({ board });
   },
 
   setBoardState: (board) => set({ board }),
+
+  updateBoard: async (todo, columnId) => {
+    await database.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
 }));
